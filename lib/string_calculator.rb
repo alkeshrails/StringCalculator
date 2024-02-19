6121678795aa93b284f1c6b484328b32ebdd5535
alkeshrails <akhileshhdev38@gmail.com>
2024-02-19 12:32:03 +0530

    Handle non-string input

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,8 @@
 # lib/string_calculator.rb
 class StringCalculator
   def self.add(numbers)
+    raise ArgumentError, "Input must be a string" unless numbers.is_a?(String)
+
     return 0 if numbers.empty?
 
   end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,10 +1,16 @@
 # spec/string_calculator_spec.rb
 require_relative '../lib/string_calculator'
 
-RSpec.describe "StringCalculator" do
+RSpec.describe StringCalculator do
+  subject { described_class }
+
   describe "#add" do
     it "returns 0 for an empty string" do
-      expect(StringCalculator.add("")).to eq(0)
+      expect(subject.add("")).to eq(0)
+    end
+
+    it "raises ArgumentError for non-string input" do
+      expect { subject.add(100) }.to raise_error(ArgumentError, "Input must be a string")
     end
   end
 end
