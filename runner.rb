0221d706e1df4d371ca6e6a4694e9c90564033c9
alkeshrails <akhileshhdev38@gmail.com>
2024-02-20 12:56:49 +0530

    Handle ARGV arguments cases

--- runner.rb
+++ runner.rb
@@ -9,9 +9,15 @@ class Runner
   end
 
   def run
+    raise 'No string passed' if ARGV.count.zero?
+    raise 'Need one string argument only' if ARGV.count != 1
+
     string = ARGV[0]
+    # The purpose of this line is to replace occurrences of the string "\\n" with the actual newline character "\n" in the given input string.
+    # In Ruby, when we pass arguments from the command line, special characters like "\n" might be represented as "\\n".
+    string = string.gsub('\\n', "\n")
     puts "sum = #{@calculator.add(string)}"
-  rescue ArgumentError => e
+  rescue => e
     puts "Error: #{e.message}"
   end
 end
